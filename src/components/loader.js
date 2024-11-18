4c0d4065e767d86c81f4f475a8157149021a6abb
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2024-11-18 11:41:01 +0100

    estilo loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,3 +1,5 @@
+import style from "./styles.module.css";
+
 export default function Loader({ item, id }) {
   if (item === null) {
     return <Container>Loading...</Container>;
@@ -6,9 +8,9 @@ export default function Loader({ item, id }) {
   if (item === undefined) {
     return <Container>Url not found {id}</Container>;
   }
-  return <Container>Redirect {item.url}</Container>;
+  return <Container>Redirecting to {item.url}...</Container>;
 }
 
 function Container({ children }) {
-  return <div>{children}</div>;
+  return <div className={style.loaderContainer}>{children}</div>;
 }
